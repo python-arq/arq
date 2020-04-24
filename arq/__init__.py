b6fa9ba8ea339258d64fdcdb0ba42299680d1fad
Samuel Colvin <s@muelcolvin.com>
2020-04-24 17:12:00 +0100

    add VERSION to __init__

--- arq/__init__.py
+++ arq/__init__.py
@@ -1,3 +1,4 @@
 from .connections import ArqRedis, create_pool  # noqa F401
 from .cron import cron  # noqa F401
+from .version import VERSION  # noqa F401
 from .worker import Retry, Worker, check_health, func, run_worker  # noqa F401
