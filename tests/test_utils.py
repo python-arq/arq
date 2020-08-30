f1ec07e14a7393c34f3f675c016d0362299237d6
Michael Curry <mike@exodusdev.com>
2020-08-30 12:27:26 -0700

    Add test to verify schema (and other) validation

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -112,3 +112,8 @@ def test_redis_settings_from_uri(uri, expected):
     assert rs.host == expected["host"]
     assert rs.port == expected["port"]
     assert rs.password == expected["password"]
+
+@pytest.mark.parametrize('uri', [("bogus://user:password@host:1234"), ("")])
+def test_redis_settings_from_uri_validation(uri):
+    with pytest.raises(AssertionError) :
+        rs = RedisSettings.from_redis_url(uri)
