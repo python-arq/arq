7a459c867f17a11338562487c28d61fb774734a4
Michael Curry <mike@exodusdev.com>
2020-08-31 07:49:22 -0700

    Satisfy linter

--- arq/connections.py
+++ arq/connections.py
@@ -40,7 +40,7 @@ class RedisSettings:
     sentinel_master: str = 'mymaster'
 
     @classmethod
-    def from_redis_url(cls, url: str, **kwargs:Any) -> 'RedisSettings':
+    def from_redis_url(cls, url: str, **kwargs: Any) -> 'RedisSettings':
         """Parse a redis: connection URI, return a new
         arq.connections.RedisSettings instance
 
