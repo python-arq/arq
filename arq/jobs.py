ae3e39cc9deb0016af0bb1a1eef6754a4dc7ebea
Ajmal Aboobacker <43377443+B3EF@users.noreply.github.com>
2021-01-10 15:16:45 +0530

    Update jobs.py

--- arq/jobs.py
+++ arq/jobs.py
@@ -17,8 +17,22 @@ safe_builtins = {
     'complex',
     'set',
     'frozenset',
+
     'slice',
 }
+class RestrictedUnpickler(pickle.Unpickler):
+
+    def find_class(self, module, name):
+        """Only allow safe classes from builtins"""
+        if module == "builtins" and name in safe_builtins:
+            return getattr(builtins, name)
+        """Forbid everything else"""
+        raise pickle.UnpicklingError("global '%s.%s' is forbidden" %
+                                     (module, name))
+
+def restricted_loads(s):
+    """Helper function analogous to pickle.loads()"""
+    return RestrictedUnpickler(io.BytesIO(s)).load()
 
 logger = logging.getLogger('arq.jobs')
 
