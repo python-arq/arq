6a5d89de0513533f0487bf4430780e1eb9990cc0
Prakash Prajapati <paxprajapati@gmail.com>
2021-07-30 13:42:11 +0545

    fix: arq is incompatible for aioredis>=2.0.0

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
         arq=arq.cli:cli
     """,
     install_requires=[
-        'aioredis>=1.1.0',
+        'aioredis>=1.1.0,<2.0.0',
         'click>=6.7',
         'pydantic>=1',
         'dataclasses>=0.6;python_version == "3.6"',
