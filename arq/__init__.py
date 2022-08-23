867fef7a897af5731c6f6f2a835bd60bb8bc8d10
Samuel Colvin <s@muelcolvin.com>
2022-08-23 12:58:40 +0100

    add version to __init__.py

--- arq/__init__.py
+++ arq/__init__.py
@@ -3,6 +3,8 @@ from .cron import cron
 from .version import VERSION
 from .worker import Retry, Worker, check_health, func, run_worker
 
+__version__ = VERSION
+
 __all__ = (
     'ArqRedis',
     'create_pool',
