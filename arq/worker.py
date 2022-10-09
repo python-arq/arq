7fb51f64cf61bb0956ae6d13f6e599a5e727be10
Piotr Janiszewski <piotr.janiszewski@kron.no>
2022-10-09 22:06:51 +0200

    Run isort

--- arq/worker.py
+++ arq/worker.py
@@ -27,16 +27,7 @@ from .constants import (
     result_key_prefix,
     retry_key_prefix,
 )
-from .utils import (
-    args_to_string,
-    import_string,
-    ms_to_datetime,
-    poll,
-    timestamp_ms,
-    to_ms,
-    to_seconds,
-    to_unix_ms,
-)
+from .utils import args_to_string, import_string, ms_to_datetime, poll, timestamp_ms, to_ms, to_seconds, to_unix_ms
 
 if TYPE_CHECKING:
     from .typing import SecondsTimedelta, StartupShutdown, WorkerCoroutine, WorkerSettingsType  # noqa F401
