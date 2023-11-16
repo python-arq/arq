beffb977e27002795b6b14976164313c9cf4a037
jeanluciano <jeanluciano@mailbox.org>
2023-11-16 14:41:15 -0600

    swapped port between redis instances

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,8 +19,8 @@ def _fix_loop(event_loop):
 @pytest.fixture
 async def arq_redis(loop):
     redis_ = ArqRedis(
-        host='127.0.0.1',
-        port=6379,
+        host='localhost',
+        port=7000,
         encoding='utf-8',
     )
 
@@ -41,8 +41,8 @@ async def unix_socket_path(loop, tmp_path):
 @pytest.fixture
 async def arq_redis_msgpack(loop):
     redis_ = ArqRedis(
-        host='127.0.0.1',
-        port=6379,
+        host='localhost',
+        port=7000,
         encoding='utf-8',
         job_serializer=msgpack.packb,
         job_deserializer=functools.partial(msgpack.unpackb, raw=False),
@@ -55,8 +55,8 @@ async def arq_redis_msgpack(loop):
 @pytest.fixture
 async def arq_redis_cluster(loop):
     settings = RedisSettings(
-        host='127.0.0.1',
-        port='5000',
+        host='localhost',
+        port=6379,
         conn_timeout=5,
         cluster_mode=True
     )
