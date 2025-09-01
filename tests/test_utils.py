af6649bb7cfafc477856d0cb3545dd5eda2be351
Nikita Ekaterinchuk <nikita.ekaterinchuk@exness.com>
2025-09-01 14:55:37 +0300

    Allow SSL support for Sentinel

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,7 +22,7 @@ def test_settings_changed():
         "RedisSettings(host='localhost', port=123, unix_socket_path=None, database=0, username=None, password=None, "
         "ssl=False, ssl_keyfile=None, ssl_certfile=None, ssl_cert_reqs='required', ssl_ca_certs=None, "
         'ssl_ca_data=None, ssl_check_hostname=False, conn_timeout=1, conn_retries=5, conn_retry_delay=1, '
-        "max_connections=None, sentinel=False, sentinel_master='mymaster', "
+        "max_connections=None, sentinel=False, sentinel_master='mymaster', sentinel_kwargs=None, "
         'retry_on_timeout=False, retry_on_error=None, retry=None)'
     ) == str(settings)
 
@@ -220,6 +220,7 @@ def test_settings_plain():
         'conn_retries': 5,
         'conn_retry_delay': 1,
         'sentinel': False,
+        'sentinel_kwargs': None,
         'sentinel_master': 'mymaster',
         'retry_on_timeout': False,
         'retry_on_error': None,
@@ -249,6 +250,7 @@ def test_settings_from_socket_dsn():
         'conn_retries': 5,
         'conn_retry_delay': 1,
         'sentinel': False,
+        'sentinel_kwargs': None,
         'sentinel_master': 'mymaster',
         'retry_on_timeout': False,
         'retry_on_error': None,
