0bfaf9b9193dc3f255538e934abd3667fda67f3b
Viicos <65306057+Viicos@users.noreply.github.com>
2026-01-29 22:13:23 +0100

    Fix fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -38,7 +38,7 @@ def test_redis_host(redis_container: RedisContainer) -> str:
 
 @pytest.fixture(scope='session')
 def test_redis_port(redis_container: RedisContainer) -> int:
-    return redis_container.get_exposed_port(redis_container.port_to_expose)
+    return redis_container.get_exposed_port(redis_container.port)
 
 
 @pytest.fixture(scope='session')
